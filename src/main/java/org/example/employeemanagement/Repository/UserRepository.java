13e5fe826f97b3b08049659bc845a53de9923e06
SarahNiyonziza <sarahniyonziza@gmail.com>
2025-11-17 10:22:13 +0200

    I did changes in UserRepository

--- src/main/java/org/example/employeemanagement/Repository/UserRepository.java
+++ src/main/java/org/example/employeemanagement/Repository/UserRepository.java
@@ -3,11 +3,11 @@ package org.example.employeemanagement.Repository;
 import org.example.employeemanagement.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-
+ // extends  jpa that will help connecting to database
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-
+// will see if the username is already exists
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
