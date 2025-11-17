8c833d858387265cdeeffdad8a060c3cf8396bac
SarahNiyonziza <sarahniyonziza@gmail.com>
2025-11-17 09:24:32 +0200

    Connections of database

--- src/main/java/org/example/employeemanagement/Repository/EmployeeRepository.java
+++ src/main/java/org/example/employeemanagement/Repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
-//package org.example.employeemanagement.Repository;
-//
-//import org.example.employeemanagement.Entity.Employee;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.Optional;
-//
-//public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-//}
+package org.example.employeemanagement.Repository;
+
+import org.example.employeemanagement.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
